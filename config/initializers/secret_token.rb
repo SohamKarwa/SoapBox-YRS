a1f8422820c93c8ab3c4212578b97e9591a10a18
Soham Karwa <sohamkarwa@gmail.com>
2013-08-15 15:35:34 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SoapBox::Application.config.secret_key_base = '772dbb27c6051114da0dddd3eca62ec61f23aec7cc00b196caaf72a71dabe5e3bd7b54c4a79b72720e67689b8635f41434fbde8518f9fa33bb7eaa97423fd9b9'
